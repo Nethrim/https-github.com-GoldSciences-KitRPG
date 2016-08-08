cdbd423ce2090b43105b402c5f554962aebb760c
John <John@45P121N>
2016-08-07 21:24:58 -0300

    * Fixed missing Item.h.

---
+++ RPG Console App/Item.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+   
+struct SItem 
+{
+	std::string	Name;
+	int			Price;
+};
+
+#define MAX_ITEM_DESCRIPTIONS 3
+
+static const SItem itemDescriptions[MAX_ITEM_DESCRIPTIONS] = {
+	{"Small HP Potion",		50},
+	{"Medium HP Potion",	100},
+	{"Large HP Potion",		150},
+};
